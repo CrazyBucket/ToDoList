f3a7767d03a9c737c4f7907e6ef736afeae5c4e5
CrazyBucket <1475743203@qq.com>
2023-04-16 00:37:30 +0800

    🎉 todolist init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Header from "./src/component/Header";
+import Input from "./src/component/Input";
+import Card from "./src/component/Card";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <LinearGradient
+      colors={["rgba(151, 255, 236, 0.3)", "rgba(128, 140, 255, 0.3)"]}
+      start={[0, 0]}
+      end={[1, 1]}
+      style={styles.container}
+    >
       <StatusBar style="auto" />
-    </View>
+      <Header />
+      <Input />
+      <Card />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
